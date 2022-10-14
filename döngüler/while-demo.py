8b9d72da5f1a7ecef974452b8ce3a698c930686d
stohma <serdartohma@hotmail.com>
2022-10-14 05:44:16 +0300

    ekle

---
+++ döngüler/while-demo.py
@@ -0,0 +1,50 @@
+sayilar = [4, 6, 9, 10, 35, 57, 89, 125, 244]
+
+# 1: sayilar listesini while ile ekrana yazdırın
+
+# for i in sayilar:
+#     print(i)
+# for ile üstteki gibi yapabiliriz.
+
+# i = 0
+# while (i < len(sayilar)):
+#     print(sayilar[i])
+#     i += 1
+
+# veya
+
+# while sayilar:
+#     print(sayilar.pop(0)) #bu da alternatif yol
+
+# 2: Başlangıç ve bitiş değerlerini kullanıcıdan alıp aradaki tüm tek sayıları ekrana yazdırın.
+
+# i=0 veya
+# baslangic = int(input('baslangıç: '))
+# bitis = int(input('bitis: '))
+#
+# i= baslangic
+# while i < bitis:
+#     i+=1
+#     if(i%2==1): # tek sayı
+#         print(i)
+
+# 3: 1-100 arasındaki sayıları azalan şekilde yazdırın
+
+# i=100
+#
+# while(i>0):
+#     print(i)
+#     i-=1
+
+# 4: kullanıcıdan alacağınız 5 sayıyı ekranda sıralı şekilde yazdırın
+
+# sayilar=[]
+#
+# i=0
+# while(i<5):
+#     sayi=int(input("sayı: "))
+#     sayilar.append(sayi)
+#     i+=1
+#
+# sayilar.sort()
+# print(sayilar)
